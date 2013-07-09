dd246845e38f3befe2743412afff39d99cd579b1
Andrew Prolov <andrewprolov@gmail.com>
2013-07-09 23:11:27 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer('256');
+var buf = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
@@ -12,7 +12,7 @@ fs.readFileSync('index.html', function(err, data) {
 });
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(buf.length));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
